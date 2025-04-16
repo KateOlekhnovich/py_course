51a147736a3428d6e97042f2974a724ebf474250
Kate O. <olekhnovich.k@gmail.com>
2025-04-16 22:08:14 +0300

    Added class work

---
+++ class_work_L1.py
@@ -0,0 +1,34 @@
+print ("hello world!")
+
+print ('rut')
+
+print ('Привет!')
+
+print ('\\')
+# \
+
+print ('hi\'you')
+# hi'you
+
+print ('hi"you')
+# hi"you
+
+print (1, 2, 4, '4 arguments')
+# 1 2 4 4 arguments
+
+print (1, 2, 3, sep='---')
+# 1---2---3
+
+print ('programming', 'essencial', 'in...Python', sep='***')
+# programming***essencial***in...Python
+
+print ('programming', 'essencial', sep='***', end='in...Python')
+#programming***essencialin...Python
+
+print ("""ldgdfgh
+... fjgdfgf
+... djfkgdf
+... """)
+# ldgdfgh
+# fjgdfgf
+# djfkgdf
